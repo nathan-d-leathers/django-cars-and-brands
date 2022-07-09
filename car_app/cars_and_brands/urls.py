b6934f5256efa95210583bba326c73b4a5f91a19
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-07-08 17:04:47 -0700

    notes

--- car_app/cars_and_brands/urls.py
+++ car_app/cars_and_brands/urls.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.urls import path, include
 
-# Create your views here.
+urlpatterns = [
+    path('/brands', view.brand_list),
+]
 
 # /brands # a list of all the car brands
 # /brands/new # form for a new car brand
 # /brands/<:id> # see a specific car brand
 # /brands/<:id>/edit # edit page for a specific car brand
+
 # /brands/<:brand_id>/cars # a list of cars for a specific car brand
 # /brands/<:brand_id>/cars/new # form for a new car under a specific car brand
 # /brands/<:brand_id>/cars/<:car_id> # see a specific car for a specific car brand
